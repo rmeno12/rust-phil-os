c511988ec22ea07e7c13a64abba08e60b84d4159
Rahul Menon <menonrahul02@gmail.com>
2025-08-03 16:07:07 -0500

    use hlt loop instead of just spinning

--- src/lib.rs
+++ src/lib.rs
@@ -53,8 +53,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
     serial_println!("[failed]\n");
     serial_println!("Error: {}\n", info);
     exit_qemu(QemuExitCode::Failed);
-    #[allow(clippy::empty_loop)]
-    loop {}
+    hlt_loop();
 }
 
 /// Entry point for `cargo test`
@@ -63,7 +62,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 pub extern "C" fn _start() -> ! {
     init();
     test_main();
-    loop {}
+    hlt_loop();
 }
 
 #[cfg(test)]
@@ -77,4 +76,10 @@ pub fn init() {
     interrupts::init_idt();
     unsafe { interrupts::PICS.lock().initialize() };
     x86_64::instructions::interrupts::enable();
+}
+
+pub fn hlt_loop() -> ! {
+    loop {
+        x86_64::instructions::hlt();
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -17,16 +17,14 @@ pub extern "C" fn _start() -> ! {
     test_main();
 
     println!("no crash");
-    #[allow(clippy::empty_loop)]
-    loop {}
+    rust_phil_os::hlt_loop();
 }
 
 #[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{info}");
-    #[allow(clippy::empty_loop)]
-    loop {}
+    rust_phil_os::hlt_loop();
 }
 
 #[cfg(test)]
