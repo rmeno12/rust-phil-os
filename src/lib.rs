2161370a3cc8e3fb84447e65a27bcfd8c96ad7f8
Rahul Menon <menonrahul02@gmail.com>
2025-08-04 14:47:00 -0500

    set up kernel heap + allocation

--- src/lib.rs
+++ src/lib.rs
@@ -10,6 +10,9 @@ use core::panic::PanicInfo;
 #[cfg(test)]
 use bootloader::{BootInfo, entry_point};
 
+extern crate alloc;
+
+pub mod allocator;
 pub mod gdt;
 pub mod interrupts;
 pub mod memory;
--- tests/heap_allocation.rs
+++ tests/heap_allocation.rs
@@ -6,51 +6,55 @@
 
 extern crate alloc;
 
+use core::panic::PanicInfo;
+
 use alloc::{boxed::Box, vec::Vec};
 use bootloader::{BootInfo, entry_point};
-use core::panic::PanicInfo;
-use rust_phil_os::{allocator, memory, println};
+use rust_phil_os::{allocator::{self, HEAP_SIZE}, hlt_loop, memory};
 use x86_64::VirtAddr;
 
-entry_point!(kernel_main);
+entry_point!(main);
 
-fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    println!("Hello World{}", "!");
-
+fn main(boot_info: &'static BootInfo) -> ! {
     rust_phil_os::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
     let mut mapper = unsafe { memory::init(phys_mem_offset) };
     let mut frame_allocator =
         unsafe { memory::BootInfoFrameAllocator::init(&boot_info.memory_map) };
-
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("kernel heap init failed");
 
-    let x = Box::new(31);
-    println!("heap allocated value at {x:p}");
-
-    let mut vec = Vec::new();
-    for i in 0..500 {
-        vec.push(i);
-    }
-    println!("vec at {:p}", vec.as_slice());
-
-    #[cfg(test)]
     test_main();
-
-    println!("no crash");
-    rust_phil_os::hlt_loop();
+    hlt_loop();
 }
 
-#[cfg(not(test))]
-#[panic_handler]
-fn panic(info: &PanicInfo) -> ! {
-    println!("{info}");
-    rust_phil_os::hlt_loop();
-}
-
-#[cfg(test)]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     rust_phil_os::test_panic_handler(info)
+}
+
+#[test_case]
+fn simple_allocation() {
+    let hv1 = Box::new(1);
+    let hv2 = Box::new(4040);
+    assert_eq!(*hv1, 1);
+    assert_eq!(*hv2, 4040);
+}
+
+#[test_case]
+fn large_vec() {
+    let n = 1000;
+    let mut vec = Vec::new();
+    for i in 0..n {
+        vec.push(i);
+    }
+    assert_eq!(vec.iter().sum::<u64>(), (n - 1) * n / 2);
+}
+
+#[test_case]
+fn many_boxes() {
+    for i in 0..HEAP_SIZE {
+        let x = Box::new(i);
+        assert_eq!(*x, i);
+    }
 }
